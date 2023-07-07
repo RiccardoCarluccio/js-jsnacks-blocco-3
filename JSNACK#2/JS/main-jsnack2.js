bfbd655e4d1985b6fe54443de5970c599e44ca8a
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 15:39:36 +0200

    end of Jsnack#2

--- JSNACK#2/JS/main-jsnack2.js
+++ JSNACK#2/JS/main-jsnack2.js
@@ -1,4 +1,6 @@
 const ARRAY_OF_NUMBERS = [];
+let oddSum  = 0;
+let evenSum = 0;
 
 for (i = 0; i < 30; i++) {
   ARRAY_OF_NUMBERS.push(Math.floor(Math.random() * 11));
@@ -7,3 +9,14 @@ for (i = 0; i < 30; i++) {
 console.log(`ARRAY_OF_NUMBERS is: ${ARRAY_OF_NUMBERS}`);
 //WORKING
 
+for (i = 0; i < ARRAY_OF_NUMBERS.length; i++) {   //.length è ovviamente 30
+  if (ARRAY_OF_NUMBERS[i] % 2 !== 0 ) {
+    oddSum += parseInt(ARRAY_OF_NUMBERS[i]);
+  } else {
+    evenSum += parseInt(ARRAY_OF_NUMBERS[i]);
+  }
+}
+
+//TEST SUMS
+console.log(`The sum of the odd numbers is: ${oddSum}`);
+console.log(`The sum of the even numbers is: ${evenSum}`);
