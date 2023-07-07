71f15c0ab383146780d3082cd412d7ee4ebf0bab
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 17:17:41 +0200

    changed array scope

--- JSNACK#3/JS/main-jsnack3.js
+++ JSNACK#3/JS/main-jsnack3.js
@@ -1,6 +1,6 @@
 const BTN_GET_ARRAY = document.getElementById("btn-get-array");
 const n = document.getElementById("user-array");
-const TEN_ELEMENTS_ARRAY = [];
+let TEN_ELEMENTS_ARRAY = [];
 
 //TEST VARs
 console.log(`n is: ${n}`);
@@ -11,16 +11,15 @@ BTN_GET_ARRAY.addEventListener('click', function arrayGenerator() {
   console.log(`User input and NUMBERS_OF_ARRAYS are both: ${NUMBERS_OF_ARRAYS}`);
 
   for (let i = 0; i < parseInt(NUMBERS_OF_ARRAYS); i++) {
+    TEN_ELEMENTS_ARRAY = [];
 
     //CREATES ARRAY CONTENT
     for (i = 0; i < 10; i++) {
       TEN_ELEMENTS_ARRAY.push(Math.floor(Math.random() * 101));
     }
 
-    console.log(`The array #${parseInt(NUMBERS_OF_ARRAYS)} is ${TEN_ELEMENTS_ARRAY}`);
+    console.log(`The array #${NUMBERS_OF_ARRAYS} is ${TEN_ELEMENTS_ARRAY}`);
   }
 })
 
-//chiaramente aggiunge ogni volta valori all'array, perchè è una const globale
-
-//ma l'array #userInput in realtà è sempre lo stesso
+//l'output è un solo array e non in numero indicato dall'utente
