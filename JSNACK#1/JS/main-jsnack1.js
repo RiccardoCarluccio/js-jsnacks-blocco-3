9ef75b4a3db1d5d5df76a6fa622397dde4b668a9
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 14:55:26 +0200

    completed test logs

--- JSNACK#1/JS/main-jsnack1.js
+++ JSNACK#1/JS/main-jsnack1.js
@@ -2,18 +2,19 @@ const BTN_GET_NUM = document.querySelector(".btn-get-num");
 const USER_NUM = document.getElementById("user-num");
 let userSum;
 
-//TEST ROUTINE #1
+//TEST PREVIOUS VARIABLES
 console.log(`USER_NUM is: ${USER_NUM}`);
 console.log(USER_NUM);
 console.log(`userSum is: ${userSum}`);
-//END TEST ROUTINE #1
+//END TEST
 
 BTN_GET_NUM.addEventListener('click', function addNum() {
   const ADDED_NUM = USER_NUM.value;
   //TEST PRREVIOUS CONST
-  console.log(ADDED_NUM);
+  console.log(`ADDED_NUM = USER_NUM.value is: ${ADDED_NUM}`);
 
-  userSum += ADDED_NUM;
+  userSum += ADDED_NUM; //da poi wrappare in un if < 50
 
-  console.log(userSum);
+  //TEST FINAL SUM
+  console.log(`userSum += ADDED_NUM is: ${userSum}`);
 })
