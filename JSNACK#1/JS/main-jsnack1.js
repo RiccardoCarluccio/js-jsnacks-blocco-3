9f9982047e275d7b5ec55ad4087d934de56d84cf
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 15:08:53 +0200

    bug fixed because of a typo probably

--- JSNACK#1/JS/main-jsnack1.js
+++ JSNACK#1/JS/main-jsnack1.js
@@ -1,6 +1,6 @@
 const BTN_GET_NUM = document.querySelector(".btn-get-num");
 const USER_NUM = document.getElementById("user-num");
-let userSum;
+let userSum = 0;
 
 //TEST PREVIOUS VARIABLES
 console.log(`USER_NUM is: ${USER_NUM}`);
@@ -13,7 +13,7 @@ BTN_GET_NUM.addEventListener('click', function addNum() {
   //TEST PREVIOUS CONST
   console.log(`ADDED_NUM = USER_NUM.value is: ${ADDED_NUM}`);
 
-  userSum += ADDED_NUM; //da poi wrappare in un if < 50
+  userSum += parseInt(ADDED_NUM); //da poi wrappare in un if < 50
 
   //TEST FINAL SUM
   console.log(`userSum += ADDED_NUM is: ${userSum}`);
