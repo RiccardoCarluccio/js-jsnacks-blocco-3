f7c9c7e085a28c9905df4774bdc9ed7210a70be3
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 17:03:59 +0200

    clarity const rename

--- JSNACK#3/JS/main-jsnack3.js
+++ JSNACK#3/JS/main-jsnack3.js
@@ -1,22 +1,23 @@
 const BTN_GET_ARRAY = document.getElementById("btn-get-array");
 const n = document.getElementById("user-array");
-const ARRAY_OF_NUMBERS = [];
+const TEN_ELEMENTS_ARRAY = [];
 
 //TEST VARs
 console.log(`n is: ${n}`);
 
 BTN_GET_ARRAY.addEventListener('click', function arrayGenerator() {
   const NUMBERS_OF_ARRAYS = n.value;
-  //TEST ARRAY QUANTITY
-  console.log(`NUMBERS_OF_ARRAYS is: ${NUMBERS_OF_ARRAYS}`);
+  //TEST PREVIOUS CONST
+  console.log(`User input and NUMBERS_OF_ARRAYS are both: ${NUMBERS_OF_ARRAYS}`);
 
   for (let i = 0; i < parseInt(NUMBERS_OF_ARRAYS); i++) {
 
     //CREATES ARRAY CONTENT
     for (i = 0; i < 10; i++) {
-      ARRAY_OF_NUMBERS.push(Math.floor(Math.random() * 101));
+      TEN_ELEMENTS_ARRAY.push(Math.floor(Math.random() * 101));
     }
-    console.log(`The array #${parseInt(NUMBERS_OF_ARRAYS)} is ${ARRAY_OF_NUMBERS}`);
+
+    console.log(`The array #${parseInt(NUMBERS_OF_ARRAYS)} is ${TEN_ELEMENTS_ARRAY}`);
   }
 })
 
