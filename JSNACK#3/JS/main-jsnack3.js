b3fd136e89c9589802f94e232731ef4eeeb2e3b2
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 16:29:33 +0200

    jsnack#3 v0.8 solution almost found

--- JSNACK#3/JS/main-jsnack3.js
+++ JSNACK#3/JS/main-jsnack3.js
@@ -1,14 +1,21 @@
-const BTN_GET_ARRAY = document.querySelector("user-array");
+const BTN_GET_ARRAY = document.getElementById("btn-get-array");
 const n = document.getElementById("user-array");
 const ARRAY_OF_NUMBERS = [];
 
+//TEST VARs
+console.log(`n is: ${n}`);
+
 BTN_GET_ARRAY.addEventListener('click', function arrayGenerator() {
-  for (i = 0; i === n; i++) {
-    
+  const ARRAY_QUANTITY = n.value;
+  //TEST ARRAY QUANTITY
+  console.log(`ARRAY_QUANTITY is: ${ARRAY_QUANTITY}`);
+
+  for (let i = 0; i < parseInt(ARRAY_QUANTITY); i++) {
+
     //CREATES ARRAY CONTENT
     for (i = 0; i < 11; i++) {
       ARRAY_OF_NUMBERS.push(Math.floor(Math.random() * 101));
     }
-    console.log(`The array #${i} is ${ARRAY_OF_NUMBERS}`);
+    console.log(`The array #${parseInt(ARRAY_QUANTITY)} is ${ARRAY_OF_NUMBERS}`);
   }
 })
