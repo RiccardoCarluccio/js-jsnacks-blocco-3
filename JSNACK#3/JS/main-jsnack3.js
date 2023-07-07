6a8fb71673a651ea2a1144a64969c0397e8322be
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-07 16:06:39 +0200

    jsnack#3 v0.5

--- JSNACK#3/JS/main-jsnack3.js
+++ JSNACK#3/JS/main-jsnack3.js
@@ -1,3 +1,14 @@
 const BTN_GET_ARRAY = document.querySelector("user-array");
 const n = document.getElementById("user-array");
+const ARRAY_OF_NUMBERS = [];
 
+BTN_GET_ARRAY.addEventListener('click', function arrayGenerator() {
+  for (i = 0; i === n; i++) {
+    
+    //CREATES ARRAY CONTENT
+    for (i = 0; i < 11; i++) {
+      ARRAY_OF_NUMBERS.push(Math.floor(Math.random() * 101));
+    }
+    console.log(`The array #${i} is ${ARRAY_OF_NUMBERS}`);
+  }
+})
